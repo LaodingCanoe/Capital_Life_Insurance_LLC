3ae22f126eedef0820f9ed814802cca93afb7083
LaodingCanoe <iskanderlife2005@gmail.com>
2024-05-25 04:21:43 +0500

    1

---
+++ Capital_Life_Insurance_LLC/CandidateAddEditPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Capital_Life_Insurance_LLC
+{
+    /// <summary>
+    /// Логика взаимодействия для CandidateAddEditPage.xaml
+    /// </summary>
+    public partial class CandidateAddEditPage : Page
+    {
+        public CandidateAddEditPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
